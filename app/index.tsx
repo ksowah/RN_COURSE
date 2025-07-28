7c74da12340ff402541e88020b54ab5e218dc44c
Kelvin Sowah <sowahkelvin640@gmail.com>
2025-07-28 23:37:49 +0000

    text input

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import LessonTwo from "@/components/lesson-02";
 import LessonThree from "@/components/lesson-03";
+import LessonFour from "@/components/lesson-04";
 import { StyleSheet, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       {/* <LessonTwo /> */}
-      <LessonThree />
+      {/* <LessonThree /> */}
+      <LessonFour />
     </View>
   );
 }
