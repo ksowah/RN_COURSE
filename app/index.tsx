8f5e7798ab98006cc28bdf4894cd09fddb1550d5
Kelvin Sowah <sowahkelvin640@gmail.com>
2025-07-28 22:46:41 +0000

    hooks

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,12 @@
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import LessonTwo from "@/components/lesson-02";
+import LessonThree from "@/components/lesson-03";
+import { StyleSheet, View } from "react-native";
 
 export default function Index() {
-
- const handlePress = () => {
-  console.log("Button Pressed")
- }
-
- const handleTouchablePress = () => {
-  console.log("Touchable Button Pressed")
- }
-
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello welcome to Tek Ocean, make sure you subscribe.</Text>
-      <Text>Hello World</Text>
-
-      <Button title="Press Me" onPress={handlePress} />
-
-      <TouchableOpacity activeOpacity={0.8} style={styles.button} onPress={handleTouchablePress} >
-        <Text style={{color: "white"}}>Touchable Button</Text>
-      </TouchableOpacity>
+      {/* <LessonTwo /> */}
+      <LessonThree />
     </View>
   );
 }
@@ -31,13 +18,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     
   },
-  text: {
-    fontWeight: "bold",
-    fontSize: 30,
-    color: "green"
-  },
-  button: {
-    padding: 6,
-    backgroundColor: "green"
-  }
+
 });
