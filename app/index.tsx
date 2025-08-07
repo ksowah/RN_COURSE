a8133d5a0dc690139de638d66d672451cdb3dd7a
Kelvin Sowah <sowahkelvin640@gmail.com>
2025-08-07 22:14:46 +0000

    setup tailwind css

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,11 @@
+import "../global.css"
+
 import LessonTwo from "@/components/lesson-02";
 import LessonThree from "@/components/lesson-03";
 import LessonFour from "@/components/lesson-04";
 import LessonFive from "@/components/lesson-05";
 import LessonSix from "@/components/lesson-06";
+import LessonSeven from "@/components/lesson-07";
 import { StyleSheet, View } from "react-native";
 
 export default function Index() {
@@ -12,7 +15,8 @@ export default function Index() {
       {/* <LessonThree /> */}
       {/* <LessonFour /> */}
       {/* <LessonFive /> */}
-      <LessonSix />
+      {/* <LessonSix /> */}
+      <LessonSeven />
     </View>
   );
 }
